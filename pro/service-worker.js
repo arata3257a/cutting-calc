a4e9ed695ed3ba226f7ca399ccbbcc61f6205408
arata3257a <arata.3257a@gmail.com>
2026-08-26 05:17:31 +0900

    Refresh Pro cache for UI improvements

--- pro/service-worker.js
+++ pro/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='machining-pro-v4';
+const CACHE='machining-pro-v5';
 const ASSETS=['./','./index.html','./manifest.json','./icon-pro.svg','./data/cwlb-verified.json'];
 
 self.addEventListener('install',e=>{

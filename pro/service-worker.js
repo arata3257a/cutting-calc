e77596e506a1381c2776eacca507c323314e69c9
arata3257a <arata.3257a@gmail.com>
2026-08-26 05:21:44 +0900

    Refresh Pro cache for resin conditions

--- pro/service-worker.js
+++ pro/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='machining-pro-v6';
+const CACHE='machining-pro-v7';
 const ASSETS=['./','./index.html','./manifest.json','./icon-pro.svg','./data/cwlb-verified.json'];
 
 self.addEventListener('install',e=>{

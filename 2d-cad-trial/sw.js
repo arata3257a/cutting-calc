21de08b7a4917d93cef4d0b4b9245bc35069a488
arata3257a <arata.3257a@gmail.com>
2026-09-20 08:46:45 +0900

    Add 7-day password protected CAD trial

---
+++ 2d-cad-trial/sw.js
@@ -0,0 +1,35 @@
+const CACHE = "easy-2d-cad-trial-v134t1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css?v=134t1",
+  "./app.js?v=134t1",
+  "./manifest.webmanifest",
+  "./icon.svg",
+  "./data/drawing.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request,{cache:["document","script","style"].includes(event.request.destination)?"no-store":"default"}).then(response => {
+      const copy=response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request,copy));
+      return response;
+    }).catch(() => caches.match(event.request))
+  );
+});

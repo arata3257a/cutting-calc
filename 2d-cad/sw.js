65446b8662257e013131e02325c4c9598c37d0c3
arata3257a <arata.3257a@gmail.com>
2026-09-20 08:13:42 +0900

    Refresh CAD v1.31 cache

--- 2d-cad/sw.js
+++ 2d-cad/sw.js
@@ -1,9 +1,9 @@
-const CACHE = "easy-2d-cad-v130";
+const CACHE = "easy-2d-cad-v131";
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=130",
-  "./app.js?v=130",
+  "./style.css?v=131",
+  "./app.js?v=131",
   "./manifest.webmanifest",
   "./icon.svg",
   "./data/drawing.json"

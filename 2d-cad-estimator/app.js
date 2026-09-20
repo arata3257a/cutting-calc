712c5a7f77308b448af693a83d3050a9cf38e55c
arata3257a <arata.3257a@gmail.com>
2026-09-21 08:26:27 +0900

    Refresh estimator service worker est7

--- 2d-cad-estimator/app.js
+++ 2d-cad-estimator/app.js
@@ -2558,7 +2558,7 @@ if ("serviceWorker" in navigator) {
   });
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("./sw.js?v=est6",{updateViaCache:"none"})
+      .register("./sw.js?v=est7",{updateViaCache:"none"})
       .then(reg=>reg.update())
       .catch(() => {});
   });

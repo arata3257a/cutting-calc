eecddacd7650968632cfcfad777b9b11ae3c2bd3
arata3257a <arata.3257a@gmail.com>
2026-09-20 18:36:07 +0900

    Bump AI 3D Maker cache to v0.2

--- card-3d-generator/sw.js
+++ card-3d-generator/sw.js
@@ -1,4 +1,4 @@
-const CACHE='ai-3d-maker-v0.1';
+const CACHE='ai-3d-maker-v0.2';
 const CORE=['./','./index.html','./style.css','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));

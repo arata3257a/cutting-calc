032256d5cf200f920f646e68c83fcf3b9aa77ecd
arata3257a <arata.3257a@gmail.com>
2026-09-21 14:34:13 +0900

    Bump service worker for hole position dimensions v1.61

--- 2d-cad/app.js
+++ 2d-cad/app.js
@@ -2569,7 +2569,7 @@ if ("serviceWorker" in navigator) {
   });
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("./sw.js?v=160",{updateViaCache:"none"})
+      .register("./sw.js?v=161",{updateViaCache:"none"})
       .then(reg=>reg.update())
       .catch(() => {});
   });

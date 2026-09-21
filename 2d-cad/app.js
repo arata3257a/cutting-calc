a251af279691618eee08b816d3d2a65ef2a3ae63
arata3257a <arata.3257a@gmail.com>
2026-09-21 12:04:54 +0900

    Bump service worker for v1.43 guided handwriting correction

--- 2d-cad/app.js
+++ 2d-cad/app.js
@@ -2569,7 +2569,7 @@ if ("serviceWorker" in navigator) {
   });
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("./sw.js?v=142",{updateViaCache:"none"})
+      .register("./sw.js?v=143",{updateViaCache:"none"})
       .then(reg=>reg.update())
       .catch(() => {});
   });
